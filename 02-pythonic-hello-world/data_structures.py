9007fa3fefdd05207aac613119e4c16ce6a0a1d1
unknown <cristianakeresztes@gmail.com>
2022-01-25 22:07:21 +0200

    un mesaj

---
+++ 02-pythonic-hello-world/data_structures.py
@@ -0,0 +1,28 @@
+
+# print(type(lista))
+# print(lista[3])
+# print(lista[1:]) #operator slicing, afiseaza elem de la ndex 1 pana la fin lista
+#print(lista[1:3]) #incl 1 excl 3
+#print(lista[1:2])
+#print(lista[0:5:2]) #elem de la index 0 la ind 5 exlcusiv, din 2 in 2
+#denumire lista[index_inceput: index final -1 pas]
+#print(lista[-1]) #ultimul element din lista
+#print(lista[-1::-1])
+#lista.append("mere")
+#lista[3] = "mere"
+#print(lista)
+#print(len(lista))
+#print(lista.index(2))
+#print(lista.index(-5))\
+#print(lista.index("-5"))
+#lista.insert(3, "element nou")
+#lista.remove(-5) #sau lista.pop(2)
+#print(lista)
+#lista.clear()
+#lista.reverse()
+#print(lista)
+#lista = [8, 2, -5, "string", 2.7]
+#print(lista)
+#lista_stringuri = ['2', '8', '21', '18', '121', '191']
+#lista_stringuri.sort()
+#print(lista_stringuri)
