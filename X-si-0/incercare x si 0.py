b14e1273e6bf9960c73206b7b562891d3a56f42f
unknown <cristianakeresztes@gmail.com>
2022-01-31 21:36:10 +0200

    ex_grup_Xsi0

---
+++ X-si-0/incercare x si 0.py
@@ -0,0 +1,16 @@
+if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ' :
+           check = 1
+       elif theBoard['7'] == theBoard['4'] == theBoard['1'] != ' ':
+           check = 1
+       elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ':
+           check = 1
+       elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ':
+           check = 1
+       elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ':
+           check = 1
+       elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ':
+          check = 1
+      elif theBoard['9'] == theBoard['6'] == theBoard['3'] != ' ':
+           check = 1
+       elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ':
+           check = 1
